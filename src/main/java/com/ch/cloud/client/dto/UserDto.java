f5d5049601ba309dec5dacd95bc3264cce9d8b67
仙_梦如尘 <zhimin711@sina.com>
2019-09-11 07:46:43 +0800

    swagger2

--- src/main/java/com/ch/cloud/client/dto/UserDto.java
+++ src/main/java/com/ch/cloud/client/dto/UserDto.java
@@ -1,5 +1,7 @@
 package com.ch.cloud.client.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -9,15 +11,19 @@ import lombok.Data;
  * @date 2019/5/28
  */
 @Data
+//@ApiModel(value = "user", description = "用户对象")
 public class UserDto {
 
+    @ApiModelProperty(hidden = true)
     private Long id;
     /**
      * 用户名
      */
+    @ApiModelProperty(name = "username", value = "登录名", required = true)
     private String username;
     /**
      * 密码
      */
+    @ApiModelProperty(name = "password", value = "密码", required = true, position = 1)
     private String password;
 }
