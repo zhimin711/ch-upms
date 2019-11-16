4d2e56bf3a263d1c46c2a865892d92c88a8b446c
麻智敏 <ZhiMinMa@sf-express.com>
2019-11-16 20:04:17 +0800

    调整role

--- src/main/java/com/ch/cloud/upms/controller/RoleController.java
+++ src/main/java/com/ch/cloud/upms/controller/RoleController.java
@@ -12,6 +12,7 @@ import com.ch.result.Result;
 import com.ch.result.ResultUtils;
 import com.ch.utils.CommonUtils;
 import com.ch.utils.DateUtils;
+import com.ch.utils.ExceptionUtils;
 import com.github.pagehelper.PageInfo;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,13 +48,14 @@ public class RoleController {
     @PostMapping
     public Result<Integer> add(@RequestBody StRole record,
                                @RequestHeader(Constants.TOKEN_USER) String username) {
-        StRole r = roleService.findByCode(record.getCode());
-        if (r != null) {
-            return Result.error(PubError.EXISTS, "角色代码已存在！");
-        } else if (CommonUtils.isEquals("0", record.getType())) {
-            return Result.error(PubError.NOT_ALLOWED, "角色类型错误！");
-        }
         return ResultUtils.wrapFail(() -> {
+            StRole r = roleService.findByCode(record.getCode());
+            if (r != null) {
+                ExceptionUtils._throw(PubError.EXISTS, "角色代码已存在！");
+            }
+            if (CommonUtils.isEquals("0", record.getType())) {
+                ExceptionUtils._throw(PubError.NOT_ALLOWED, "角色类型错误！");
+            }
             record.setCreateBy(username);
             return roleService.save(record);
         });
@@ -62,10 +64,10 @@ public class RoleController {
     @PutMapping("{id}")
     public Result<Integer> edit(@PathVariable Long id, @RequestBody StRole record,
                                 @RequestHeader(Constants.TOKEN_USER) String username) {
-        if (CommonUtils.isEquals("0", record.getType())) {
-            return Result.error(PubError.NOT_ALLOWED, "角色类型错误！");
-        }
         return ResultUtils.wrapFail(() -> {
+            if (CommonUtils.isEquals("0", record.getType())) {
+                ExceptionUtils._throw(PubError.NOT_ALLOWED, "角色类型错误！");
+            }
             record.setUpdateBy(username);
             record.setUpdateAt(DateUtils.current());
             return roleService.update(record);
