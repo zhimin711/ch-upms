e5d2259ea17440a656e344d3a41c5ad31bc204ad
仙_梦如尘 <zhimin711@sina.com>
2022-02-12 13:06:52 +0800

    调整super admin permissions

--- src/main/java/com/ch/cloud/upms/controller/RoleController.java
+++ src/main/java/com/ch/cloud/upms/controller/RoleController.java
@@ -115,14 +115,16 @@ public class RoleController {
         return ResultUtils.wrapList(() -> {
             Long rid = roleId;
             Role role = roleService.getById(roleId);
+            String typesStr2 = typesStr;
             if (role == null) {
                 ExceptionUtils._throw(PubError.NOT_EXISTS);
             } else if (CommonUtils.isEquals(StatusS.DISABLED, role.getStatus())) {
                 ExceptionUtils._throw(PubError.INVALID, "角色：" + roleId);
             } else if (CommonUtils.isEquals("SUPER_ADMIN", role.getCode())) {
                 rid = 0L;
+                typesStr2 = "3,4";
             }
-            List<String> types = StringUtilsV2.splitStrAndDeDuplication(Separator.COMMA_SIGN, typesStr);
+            List<String> types = StringUtilsV2.splitStrAndDeDuplication(Separator.COMMA_SIGN, typesStr2);
             if (types.isEmpty()) {
                 types.add("3");
             }
