76deda9551c0d5d3ea6dfba8eb00f15556c361cc
仙_梦如尘 <zhimin711@sina.com>
2022-05-12 00:50:26 +0800

    delete old dao interface

--- dao/src/main/java/com/ch/cloud/upms/mapper/BtUploadFileMapper.java
+++
@@ -1,8 +0,0 @@
-/*
-package com.ch.cloud.upms.mapper;
-
-import com.ch.cloud.upms.model.BtUploadFile;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface BtUploadFileMapper extends Mapper<BtUploadFile> {
-}*/
