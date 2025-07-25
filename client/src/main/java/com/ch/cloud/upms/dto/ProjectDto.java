c470d2a033465742f7e9680167c8d3967e9394d9
zhimin <zhimin711@sina.com>
2025-07-25 09:10:08 +0800

    feat(client): 在 ProjectDto 中添加 tenantId 字段并优化注释
    - 在 ProjectDto 中新增 tenantId 字段，用于存储租户 ID
    - 为所有字段添加了 Swagger 注释，提高了代码的可读性和文档性
    -调整了字段的顺序，使代码结构更加清晰

--- client/src/main/java/com/ch/cloud/upms/dto/ProjectDto.java
+++ client/src/main/java/com/ch/cloud/upms/dto/ProjectDto.java
@@ -3,6 +3,8 @@ package com.ch.cloud.upms.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * 描述：
  *
@@ -12,23 +14,37 @@ import lombok.Data;
 @Data
 @Schema(description = "项目DTO")
 public class ProjectDto {
-
+    
     @Schema(description = "项目ID")
     private Long id;
+    
     /**
      * 代码
      */
     private String code;
+    
     @Schema(description = "项目名称")
     private String name;
+    
+    
+    /**
+     * 租户ID
+     */
+    @Schema(description = "租户ID")
+    private Long tenantId;
+    
     /**
      * 租户名称
      */
+    @Schema(description = "租户名称")
     private String tenantName;
+    
     /**
      * 负责人
      */
+    @Schema(description = "负责人")
     private String manager;
+    
     @Schema(description = "项目描述")
     private String description;
 }
